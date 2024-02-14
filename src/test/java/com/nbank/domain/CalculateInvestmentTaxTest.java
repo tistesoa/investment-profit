a2a45e1e74d873419e08a79e3917857426e15e7f
tiagosoares <tistesoa@gmail.com>
2024-02-14 18:12:36 -0300

    finish unit test

--- src/test/java/com/nbank/domain/CalculateInvestmentTaxTest.java
+++ src/test/java/com/nbank/domain/CalculateInvestmentTaxTest.java
@@ -114,4 +114,51 @@ public class CalculateInvestmentTaxTest {
         var result = calculateInvestmentTax.calculate(input);
         assertIterableEquals(expected, result);
     }
+
+
+    @Test
+    public void case7() {
+        calculateInvestmentTax = new CalculateInvestmentTaxImpl();
+        List<BigDecimal> expected = new ArrayList<>();
+        expected.add(BigDecimal.ZERO);
+        expected.add(BigDecimal.ZERO);
+        expected.add(BigDecimal.ZERO);
+        expected.add(BigDecimal.ZERO);
+        expected.add(new BigDecimal("3000.00"));
+        expected.add(BigDecimal.ZERO);
+        expected.add(BigDecimal.ZERO);
+        expected.add(new BigDecimal("3700.00"));
+        expected.add(BigDecimal.ZERO);
+
+        List<Operation> input = new ArrayList<>();
+        input.add(new Operation(Type.BUY,new BigDecimal("10.00"),10000));
+        input.add(new Operation(Type.SELL,new BigDecimal("2.00"),5000));
+        input.add(new Operation(Type.SELL,new BigDecimal("20.00"),2000));
+        input.add(new Operation(Type.SELL,new BigDecimal("20.00"),2000));
+        input.add(new Operation(Type.SELL,new BigDecimal("25.00"),1000));
+        input.add(new Operation(Type.BUY,new BigDecimal("20.00"),10000));
+        input.add(new Operation(Type.SELL,new BigDecimal("15.00"),5000));
+        input.add(new Operation(Type.SELL,new BigDecimal("30.00"),4350));
+        input.add(new Operation(Type.SELL,new BigDecimal("30.00"),650));
+        var result = calculateInvestmentTax.calculate(input);
+        assertIterableEquals(expected, result);
+    }
+
+
+    @Test
+    public void case8() {
+        calculateInvestmentTax = new CalculateInvestmentTaxImpl();
+        List<BigDecimal> expected = new ArrayList<>();
+        expected.add(BigDecimal.ZERO);
+        expected.add(new BigDecimal("80000.00"));
+        expected.add(BigDecimal.ZERO);
+        expected.add(new BigDecimal("60000.00"));
+        List<Operation> input = new ArrayList<>();
+        input.add(new Operation(Type.BUY,new BigDecimal("10.00"),10000));
+        input.add(new Operation(Type.SELL,new BigDecimal("50.00"),10000));
+        input.add(new Operation(Type.BUY,new BigDecimal("20.00"),10000));
+        input.add(new Operation(Type.SELL,new BigDecimal("50.00"),10000));
+        var result = calculateInvestmentTax.calculate(input);
+        assertIterableEquals(expected, result);
+    }
 }
